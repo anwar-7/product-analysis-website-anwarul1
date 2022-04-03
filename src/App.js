f8c7a2e6d996f8642651f5449d61a9aff491bf79
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-04-04 05:52:58 +0600

    update home with product img and text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Dashboard from './components/Dashboard/Dashboard';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import book from '../../Assets/image/book.png';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-center">This is home bro...!</h1>
-    </div>
+    <>
+      <div className="home-container">
+        <div className="text-container">
+          <h1>Your next Book</h1>
+          <h1>Your bast Book</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            eligendi cum repellendus, voluptatum alias magnam illo quas
+            veritatis ipsum dicta sequi tempora maxime vero quae, libero,
+            laudantium sint et minima!
+          </p>
+          <div className="btn-live-demo">
+            <Button className="text-center" variant="primary">
+              Live Demo
+            </Button>{' '}
+          </div>
+        </div>
+        <div className="product-container">
+          <img src={book} alt="Habluder jonno programming By Jhankar Mahbub" />
+        </div>
+      </div>
+      <div>
+        <h1 className="text-center my-5">Customer Reviews</h1>
+      </div>
+    </>
   );
 };
 
